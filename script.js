c426c5d55834a524aa1aa8dd241e38b6b258d837
Darya Shavel <darya_shavel98@mail.ru>
2020-03-28 23:10:45 +0300

    feat: add some shanges

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ let menu = document.querySelector('.navigation');
 menu.addEventListener('click' , (event) => {
     menu.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
-}) 
+});
+
+//scroll
+/*
+document.addEventListener('scroll',onScroll);
+function onScroll(){
+    const curPos = window.scrollY;
+    const divs = document.querySelector('.wrapper main div');
+    const links = document.querySelectorAll('.navigation>a');
+
+    divs.forEach((el)=>{
+        if(el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight)  > curPos ){
+            links.forEach((a)=>{
+                a.classList.remove('active__link');
+                if(el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+                    a.classList.add('active__link');
+                };
+            });
+        };
+    });
+};
+*/
 //screen off
 let vertical = document.querySelector('.button__phone1');
 vertical.addEventListener('click', function(n){
@@ -31,8 +52,8 @@ let buttons = document.querySelectorAll('.button__navigation .button');
 let imageContainer = document.querySelector('.column__img');
 let portfolioImages = document.querySelectorAll('.column__img .portfolio_item');
     portfolioButton.addEventListener('click', (e) => {
-        buttons.forEach(tag => tag.classList.remove('selected'));
-        e.target.classList.add('selected');
+        buttons.forEach(tag => tag.classList.remove('button__active'));
+        e.target.classList.add('button__active');
         portfolioImages.forEach(img => img.style.order = Math.floor(Math.random() - 0.5));
         });
 
@@ -76,7 +97,30 @@ function showSlides(n) {
 }
 
 //form 
+const button = document.querySelector('.submit');
+const closeButton = document.getElementById('close-btn');
+const messageForm = document.querySelector('.form');
+button.addEventListener('click', function(event){
+    if(messageForm.checkValidity()){
+        event.preventDefault();
+        document.querySelector('.message-block').classList.add('message-active');
+        let subject = document.getElementById('subject').value;
+        if(!subject){
+            subject = 'Без темы';
+        };
+        let description =document.getElementById('description').value;
+        if(!description){
+            description = 'Без описания';
+        };
+        document.querySelector('.result').innerText = `\nТема: ${subject}\nОписание: ${description}`; 
+    };
+});
 
+closeButton.addEventListener('click', function(){
+    messageForm.reset();
+    document.querySelector('.message-block').querySelector('.result').innerText = '';
+    document.querySelector('.message-block').classList.remove('message-active');
+});
 
 
 
